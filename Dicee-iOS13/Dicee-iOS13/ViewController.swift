9e25b600ce10907a054534fb05085cb4df1bcf35
jrgarrigues <jgarr16@users.noreply.github.com>
2021-07-19 13:42:06 -0700

    finished the dice game

--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -13,24 +13,19 @@ class ViewController: UIViewController {
     // Created the IBOutlets by Control-dragging from the two die images in the Main.storyboard to here - this created the linkage between the UI and the code
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
-    
-    var leftDiceNumber = 0
-    var rightDiceNumber = 5
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-   
+
+    // Created the IBAction function to simulate the action of rolling the dice when the "roll" button is pressed
     @IBAction func rollButtonPressed(_ sender: UIButton) {
        
+        // Using an array to hold the dice images
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
         
-        // making an array to hold the dice
-        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
-        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
-        
-        leftDiceNumber = leftDiceNumber + 1
-        rightDiceNumber = rightDiceNumber - 1
+        // randomly change the individual die images
+        // can randomize the index by one of two ways:
+        // 1. diceArray[Int.random(in: 0...5)]
+        // 2. diceArray.randomElement()
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
         
     }
     
--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -5,7 +5,7 @@
  everything that goes in between these delimiters
  will be considered a comment */
 
-print("Hello \(2+3) World")
+//print("Hello \(2+3) World")
 
 /* we can run little sub-comnmands within a text string
  - a.k.a. a string interporlation - by doing a backslash
@@ -13,22 +13,37 @@ print("Hello \(2+3) World")
 
 
 // variables
-var a = 5
-var b = 8
-var c = a
-a = b
-b = c
+//var a = 5
+//var b = 8
+//var c = a
+//a = b
+//b = c
 
-print("A is \(a).")
-print("B is \(b).")
+//print("A is \(a).")
+//print("B is \(b).")
 
 // arrays
-import Foundation
-var friends = ["angela","jack","philipp"]
-print(friends[0].capitalized)
+//import Foundation
+//var friends = ["angela","jack","philipp"]
+//print(friends[0].capitalized)
 
 // array exercise - I was overthinking this one.
-var numbers = [45,73,195,53]
+//var numbers = [45,73,195,53]
 
-var computedNumbers = [numbers[0]*numbers[1],numbers[1]*numbers[2],numbers[2]*numbers[3],numbers[3]*numbers[0]]
-print(computedNumbers)
+//var computedNumbers = [numbers[0]*numbers[1],numbers[1]*numbers[2],numbers[2]*numbers[3],numbers[3]*numbers[0]]
+//print(computedNumbers)
+
+func exercise() {
+    let letterArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    let a = letterArray[Int.random(in: 0...25)]
+    let b = letterArray[Int.random(in: 0...25)]
+    let c = letterArray[Int.random(in: 0...25)]
+    let d = letterArray[Int.random(in: 0...25)]
+    let e = letterArray[Int.random(in: 0...25)]
+    let f = letterArray[Int.random(in: 0...25)]
+    let password = "\(a)\(b)\(c)\(d)\(e)\(f)"
+    print(password)
+}
+
+exercise()
+
