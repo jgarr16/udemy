405ff2fcb6a49ac96d5e3d70a0bb9937d68dbcaf
jrgarrigues <jgarr16@users.noreply.github.com>
2021-07-30 21:59:04 -0700

    playing in the playground

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -55,31 +55,62 @@
 //
 //print(multi)
 
+//
+//func randver(selection: Int) {
+//
+//    func nest(selection: Int){
+//        if selection == 5 {
+//            print("5")
+//        }
+//        if selection == 4 {
+//            print("4")
+//        }
+//        if selection == 3 {
+//            print("3")
+//        }
+//        if selection == 2 {
+//            print("2")
+//        }
+//        if selection == 1 {
+//            print("1")
+//        }
+//    }
+//
+//    nest(selection: selection)
+//}
+//
+//let selection = Int.random(in: 1...5)
+//randver(selection: selection)
 
-func randver(selection: Int) {
 
-    func nest(selection: Int){
-        if selection == 5 {
-            print("5")
-        }
-        if selection == 4 {
-            print("4")
-        }
-        if selection == 3 {
-            print("3")
-        }
-        if selection == 2 {
-            print("2")
-        }
-        if selection == 1 {
-            print("1")
-        }
-    }
- 
-    nest(selection: selection)
+//Don't change this code:
+func calculator() {
+  let a = Int(readLine()!)! //First input
+  let b = Int(readLine()!)! //Second input
+  
+  add(n1: a, n2: b)
+  subtract(n1: a, n2: b)
+  multiply(n1: a, n2: b)
+  divide(n1: a, n2: b)
+  
 }
 
-let selection = Int.random(in: 1...5)
-randver(selection: selection)
+//Write your code below this line to make the above function calls work.
+func add(n1: Int,n2: Int) {
+    let added=n1+n2
+    print(added)
+}
+func subtract(n1: Int,n2: Int) {
+    let subtracted=n1-n2
+    print(subtracted)
+}
+func multiply(n1: Int,n2: Int) {
+    let multiplied=n1*n2
+    print(multiplied)
+}
+func divide(n1: Int,n2: Int) {
+    let divided=Double(n1)/Double(n2)
+    print(divided)
+}
 
 
