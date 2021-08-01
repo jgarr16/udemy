e5e94e034115e5736eaee87773e916cf8229f2d0
jrgarrigues <jgarr16@users.noreply.github.com>
2021-07-31 23:37:07 -0700

    updated if/else to switch statement

--- else_if.playground/Contents.swift
+++ else_if.playground/Contents.swift
@@ -1,15 +1,28 @@
 import UIKit
 
 
+//func loveCalculator() {
+//    let loveScore = Int.random(in: 0...100)
+//    if loveScore>=80 {
+//        print("You love each other like Kanye loves Kanye (i.e., \(loveScore))!")
+//    } else if loveScore>=40 {
+//        print("You go together like Code and Mentos (i.e., \(loveScore))!")
+//    } else {
+//        print("You'll be forever alone (i.e., \(loveScore))!")
+//    }
+//}
+
 func loveCalculator() {
     let loveScore = Int.random(in: 0...100)
-    if loveScore>=80 {
+    switch loveScore {
+    case 80...:
         print("You love each other like Kanye loves Kanye (i.e., \(loveScore))!")
-    } else if loveScore>=40 {
+    case 40..<80:
         print("You go together like Code and Mentos (i.e., \(loveScore))!")
-    } else {
+    case ..<40:
         print("You'll be forever alone (i.e., \(loveScore))!")
+    default:
+        print("Error")
     }
 }
-
 loveCalculator()
