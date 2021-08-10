5473e5edd4a193de61a3ebe3f42878cd599fa729
jrgarrigues <jgarr16@users.noreply.github.com>
2021-08-09 22:21:49 -0700

    more learning oop for oogbb

--- Instruments.playground/oogbb.playground/Pages/player.xcplaygroundpage/Contents.swift
+++ Instruments.playground/oogbb.playground/Pages/player.xcplaygroundpage/Contents.swift
@@ -9,6 +9,7 @@ class Player {
     var ties: Int = 0
     var plays: Int = 0
     var noun: String = ""
+    var result: String = ""
     
     // an initializer is like a constructor; it's used to initialize a property
     func win() {
@@ -23,22 +24,17 @@ class Player {
         ties += 1
         plural(noun:"tie",count:Int("\(ties)")!)
     }
-    func plural(noun: String, count: Int) -> String {
+    func plural(noun: String, count: Int) {
         // determine the proper singular or pluralized ending for a noun based on the number of times an event has occured
         // reference: https://www.englishgrammar101.com/module-1/nouns/lesson-3/singular-and-plural-nouns-1
         // check out this one for maturity - https://github.com/joshualat/Pluralize.swift
-        if count != 1 {
-            // noun.suffix(2) == 'sh' || 'ch'
-            // return "\(noun)s"
-            if noun == "win" {self.noun = "wins"}
-            if noun == "lose" {self.noun = "loses"}
-            if noun == "tie" {self.noun = "ties"}
-        } else {
-            self.noun = noun
-//            if noun == "win" {return "win"}
-//            if noun == "lose" {return "lose"}
-//            if noun == "tie" {return "tie"}
-        }
+        if count != 1 && noun == "win" {self.noun = "wins"}
+        else if count != 1 && noun == "lose" {self.noun = "losses"}
+        else if count != 1 && noun == "tie" {self.noun = "ties"}
+        else if count == 1 && noun == "win" {self.noun = "win"}
+        else if count == 1 && noun == "lose" {self.noun = "loss"}
+        else if count == 1 && noun == "tie" {self.noun = "tie"}
+        print(noun,count)
     }
 }
 
@@ -49,14 +45,19 @@ home.name = "John"
 let visitor: Player = Player()
 visitor.name = "Kyong"
 
-visitor.win()
-home.lose()
-visitor.tie()
-home.tie()
+//visitor.win()
+//home.lose()
+//visitor.tie()
+//home.tie()
+
+home.noun
+home.win()
+//home.win()
 
 //print("This player's name is \(home.name).")
-print("\(home.name) has \(home.wins) \(home.noun), \(home.loses) loses, and \(home.ties) ties.")
-print()
+print("\(home.name) has \(home.wins) \(home.plural(noun:"win",count:Int("\(home.wins)")!)), \(home.loses) \(home.noun), and \(home.ties) \(home.noun).")
+//print()
 //print("This player's name is \(visitor.name).")
-print("\(visitor.name) has \(visitor.wins) wins, \(visitor.loses) loses, and \(visitor.ties) ties.")
+//print("\(visitor.name) has \(visitor.wins) wins, \(visitor.loses) loses, and \(visitor.ties) ties.")
 
+home.noun
