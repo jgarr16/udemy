785eaf4a7a5ab21315de7b6e9c6715f79d3dc104
jrgarrigues <jgarr16@users.noreply.github.com>
2021-08-14 15:17:11 -0700

    initial build for classes in oogbb

--- oogbb/oogbb/ViewController.swift
+++ oogbb/oogbb/ViewController.swift
@@ -4,9 +4,11 @@
 //
 //  Created by John Garrigues on 8/6/21.
 //
-
+import UIKit
 import Foundation
 // define the Player class
+
+class ViewController: UIViewController {
 
 // class for the people playing gawi-bawi-bo
 class Player {
@@ -60,19 +62,20 @@ func applicationDidFinishLaunching() {
     bo.beatBy = "gawi"
 }
 
-let a: Player = Player()
-a.engName = "John"
-let b: Player = Player()
-b.engName = "Kyong"
+//let a: Player = Player()
+//a.engName = "John"
+//let b: Player = Player()
+//b.engName = "Kyong"
 
 
 
 
 
 // using ternary conditional operator, which takes the form (question ? answer1 : answer2), to pluralize/singluarize adverbs
-print("\(a.engName) has \(a.wins)" + (a.wins != 1 ? " wins, " : " win, ") + "\(a.loses)" + (a.loses != 1 ? " losses, " : " loss, ") + "and \(a.ties)" + (a.ties != 1 ? " ties." : " tie."))
-print("\(b.engName) has \(b.wins)" + (b.wins != 1 ? " wins, " : " win, ") + "\(b.loses)" + (b.loses != 1 ? " losses, " : " loss, ") + "and \(b.ties)" + (b.ties != 1 ? " ties." : " tie."))
-
-bo.hanName
-gawi.hanName
-bawi.hanName
+//print("\(a.engName) has \(a.wins)" + (a.wins != 1 ? " wins, " : " win, ") + "\(a.loses)" + (a.loses != 1 ? " losses, " : " loss, ") + "and \(a.ties)" + (a.ties != 1 ? " ties." : " tie."))
+//print("\(b.engName) has \(b.wins)" + (b.wins != 1 ? " wins, " : " win, ") + "\(b.loses)" + (b.loses != 1 ? " losses, " : " loss, ") + "and \(b.ties)" + (b.ties != 1 ? " ties." : " tie."))
+//
+//bo.hanName
+//gawi.hanName
+//bawi.hanName
+}
